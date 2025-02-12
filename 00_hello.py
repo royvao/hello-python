d094b0c5a2a9bc2494855ec515875d1c7dabc009
royvao <royvao@gmail.com>
2025-02-12 01:34:39 +0100

    day 1

---
+++ 00_hello.py
@@ -0,0 +1,23 @@
+#Hola mundo
+print("Hello World!")
+print('Hello World!')
+
+
+"""
+Comentario 
+multilínea
+"""
+
+'''
+Otro 
+Comentario
+multilínea
+'''
+
+# Consulta tipo de dato
+print(type("Soy un dato str")) # Tipo 'str'
+print(type(5)) # Tipo 'int'
+print(type(1.5)) # Tipo 'float'
+print(type(1 + 2j)) # Tipo 'complex'
+print(type(True)) # Tipo 'bool'
+ 
---
+++ 01_variables.py
@@ -0,0 +1,16 @@
+#Variables
+
+my_variable_string = 'My String variable'
+print(my_variable_string)
+
+my_variable_int = 6
+print(my_variable_int)
+
+my_variable_int_to_str = str(my_variable_int)
+print(my_variable_int_to_str)
+print(type(my_variable_int_to_str))
+
+my_bool_variable = True
+print(my_bool_variable)
+
+print(my_variable_string, my_variable_int_to_str, my_bool_variable)
